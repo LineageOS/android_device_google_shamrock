27ebb44cf55b1ad578664d84d2f941f0b7d1b799
AdrianDC <radian.dc@gmail.com>
2016-12-06 18:43:21 +0000

    shamrock: liblights: LED brightness LibLights control
     * Add the brightness features to the LibLights.
    
    Change-Id: Ie097c85c7c31faf591c6d273c1fa15722ce9e74d

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -19,7 +19,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := lights.c
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_SHARED_LIBRARIES := liblog
 
 LOCAL_MODULE := lights.msm8952
 LOCAL_MODULE_TAGS := optional
--- liblight/lights.c
+++ liblight/lights.c
@@ -279,7 +279,35 @@ set_light_notifications(UNUSED struct light_device_t *dev,
         const struct light_state_t *state)
 {
     pthread_mutex_lock(&g_lock);
+
+    unsigned int brightness;
+    unsigned int color;
+    unsigned int rgb[3];
+
     g_notification = *state;
+
+    // If a brightness has been applied by the user
+    brightness = (g_notification.color & 0xFF000000) >> 24;
+    if (brightness > 0 && brightness < 0xFF) {
+
+        // Retrieve each of the RGB colors
+        color = g_notification.color & 0x00FFFFFF;
+        rgb[0] = (color >> 16) & 0xFF;
+        rgb[1] = (color >> 8) & 0xFF;
+        rgb[2] = color & 0xFF;
+
+        // Apply the brightness level
+        if (rgb[0] > 0)
+            rgb[0] = (rgb[0] * brightness) / 0xFF;
+        if (rgb[1] > 0)
+            rgb[1] = (rgb[1] * brightness) / 0xFF;
+        if (rgb[2] > 0)
+            rgb[2] = (rgb[2] * brightness) / 0xFF;
+
+        // Update with the new color
+        g_notification.color = (rgb[0] << 16) + (rgb[1] << 8) + rgb[2];
+    }
+
     handle_speaker_light_locked(dev);
     pthread_mutex_unlock(&g_lock);
 
@@ -374,7 +402,7 @@ struct hw_module_t HAL_MODULE_INFO_SYM = {
     .version_major = 1,
     .version_minor = 0,
     .id = LIGHTS_HARDWARE_MODULE_ID,
-    .name = "Storm Lights Module",
+    .name = "Shamrock Lights Module",
     .author = "The CyanogenMod Project",
     .methods = &lights_module_methods,
 };
