7265dbaa98fc7ef154e3f9f034b6c4f72214ff52
Rygebin <kaankulahli@gmail.com>
2016-12-09 19:18:04 +0000

    shamrock: Add audio/media configs to device tree
    * changed correct directory paths for Audio and Media
    
    Change-Id: I8948fcc9097ad35392bcbb714f4a25b5fb266964

--- product/media.mk
+++ product/media.mk
@@ -1,4 +1,6 @@
 # Media
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
