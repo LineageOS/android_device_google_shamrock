bcc15bc2edf98b98288019300cc7236e804f30ca
Rygebin <kaankulahli@gmail.com>
2016-12-28 17:31:20 +0000

    shamrock: Change to lineage.mk
    --> https://review.lineageos.org/#/c/153997/2
    
    Change-Id: If1bfa65a1ffdee22d6d4008cb1a2f9378726adaf

--- lineage.mk
+++ lineage.mk
@@ -14,13 +14,13 @@
 
 $(call inherit-product, device/google/shamrock/full_shamrock.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8952
 
-PRODUCT_NAME := cm_shamrock
+PRODUCT_NAME := lineage_shamrock
 BOARD_VENDOR := google
 PRODUCT_DEVICE := shamrock
 
