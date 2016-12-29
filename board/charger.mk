ab41f87a93ea3637ddd8d381192c441c80d736a8
Rygebin <kaankulahli@gmail.com>
2016-12-29 20:49:15 +0000

    shamrock: Add libhealthd library from CAF
    Change-Id: I7894721981298163c46cb900c16f2b2191f88c6e

--- board/charger.mk
+++ board/charger.mk
@@ -1,6 +1,6 @@
 # Charger
-BOARD_CHARGER_DISABLE_INIT_BLANK := true
-
-# Don't use the CM charger
 WITH_CM_CHARGER := false
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BOARD_HAL_STATIC_LIBRARIES += libhealthd.msm8952
+BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(DEVICE_PATH)/charger/images
 
